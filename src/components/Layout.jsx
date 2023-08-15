69569c08ac826df197e3b35efefc92c192521258
Sebastian Vidales Claros <vidales14sebastian@gmail.com>
2023-08-15 15:58:41 -0500

    Creando el darkmode a la aplicación

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,32 +1,53 @@
 import { Outlet } from "react-router-dom";
+import logo from "../images/logo.png";
 
 function Layout() {
   const date = new Date().getFullYear();
-  
+  const handleToogle = (e) => {
+    document.documentElement.classList.toggle("dark")
+    console.log(e.target.checked);
+  }
   return (
     <>
-      <div className="bg-gray-300 py-4">
-        <div className="container mx-auto flex justify-between">
-          <h1 className="text-blue-900 text-center text-4xl font-extrabold">
-            BBC BURGER
-          </h1>
-          <nav className="flex items-center gap-4">
-            <a className="uppercase font-bold hover:text-blue-900" href="">
+      <header className="dark:bg-oscuro bg-perla py-4">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="flex items-center">
+            <img className="w-12 h-12 mr-2" src={logo} alt="imagen logo" />
+            <h1 className="text-blue-900 dark:text-snow text-center font-breeSerif text-2xl font-bold">
+              BBC BURGER
+            </h1>
+          </div>
+          <nav className="flex items-center gap-4 dark:text-snow
+           font-breeSerif">
+            <a className="uppercase hover:text-blue-900" href="">
               Home
             </a>
-            <a className="uppercase font-bold hover:text-blue-900" href="">
+            <a className="uppercase hover:text-blue-900" href="">
               Menú
             </a>
-            <a className="uppercase font-bold hover:text-blue-900" href="">
+            <a className="uppercase hover:text-blue-900" href="">
               Servicios
             </a>
+            <a className="uppercase hover:text-blue-900" href="">
+              Ubicación
+            </a>
+            <a className="uppercase hover:text-blue-900" href="">
+              Contáctanos
+            </a>
           </nav>
-          <button className="uppercase font-bold">Contactanos</button>
+          <div className="flex items-center">
+            <label className="relative inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" className="sr-only peer" onChange={handleToogle}/>
+              <div className="w-11 h-6 bg-gray200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue300 dark:peer-focus:ring-blue800 rounded-full peer dark:bg-gray700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-snow after:border-gray300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray600 peer-checked:bg-blue600"></div>
+              <span className="ml-3 text-sm font-medium text-gray-900 dark:text-snow">
+                Toggle me
+              </span>
+            </label>
+          </div>
         </div>
-      </div>
+      </header>
       <main>
         <Outlet />
-        {/* <h1>HOLA MUNDO</h1> */}
       </main>
       <footer className="bg-gray-300 py-6 mt-10">
         <p className="text-center uppercase font-bold text-sm">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    fontFamily: {
+      breeSerif: ["Bree Serif", "serif"],
+      gabriela: ["Gabriela", "serif"],
+      quicksand: ["Quicksand", "sans-serif"],
+      roboto: ["Roboto Mono", "monospace"],
+      saira: ["Saira", "sans-serif"],
+    },
+    colors: {
+      oscuro: "#071E22",
+      verde: "#1D7874",
+      amarillo: "#EDAE49",
+      perla: "#E7DFC6",
+      rojo: "#EE2E31",
+      snow: "#FFF9FB",
+      azul: "#3F84E5",
+      gray200: "#e5e7eb",
+      gray300: "#d1d5db",
+      gray600: "#4b5563",
+      gray700: "#374151",
+      blue300: "#93c5fd",
+      blue600: "#0284c7",
+      blue900: "#1e3a8a",
+    },
   },
   plugins: [],
-}
+};
 
+// F4C095
+// 679289
+// EDAE49 amarillo
+// E7DFC6 perla
+// FFF9FB snow
